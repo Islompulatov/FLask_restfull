79d672c03ac6c3e7c4f02082be1d303dfdefcd62
Islompulatov <98391405+Islompulatov@users.noreply.github.com>
2022-07-14 21:34:52 +0200

    flask with sql

--- src/app.py
+++ src/app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import flask_sqlalchemy
 from routes.homes.route import HomeRoute, HomeRouteWithId
+from utils.db import db
 
-app = Flask(__name__)
-api = Api(app)
 
-api.add_resource(HomeRoute, '/')
-api.add_resource(HomeRouteWithId, '/<string:id>')
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sql"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    api = Api(app)
+    db.init_app(app) # Initialize the database
+    db.create_all(app=app) # Create tables
+    api.add_resource(HomeRoute, '/')
+    api.add_resource(HomeRouteWithId, '/<string:id>')
+    return app
 
-if __name__ == '__main__':
-    app.run(debug=True)
--- src/routes/homes/route.py
+++ src/routes/homes/route.py
@@ -1,16 +1,19 @@
+import re
 from flask_restful import Resource, Api
 from flask import request
 import uuid
+from utils.models.user import User
+from utils.db import db
 
 from numpy import append
 data = []
 
 class HomeRoute(Resource):
     def get(self):
-        print(request)
-        return {'data': data}
+        users = db.session.query(User).all()
+        return {'data': users}
     def post(self):
-        id = str(uuid.uuid4)
+        id = str(uuid.uuid4())
         name = request.form["name"]
         last_name = request.form["last_name"]
         email = request.form["email"]
@@ -21,7 +24,7 @@ class HomeRoute(Resource):
 
 def find_object_by_id(id):
     for data_object in data:
-        if data_object.id = id:
+        if data_object["id"] = id:
             return data_object
         else:
             return None            
@@ -32,3 +35,14 @@ class HomeRouteWithId(Resource):
             return {"data":data_object}
         else:
             return {"data": "Not Found"},404
+
+    def put(self,id):
+        data_object = find_object_by_id(id)
+        if(data_object):
+            data_object["name"] = request.form["name"]
+            data_object["last_name"] = request.form['last_name']
+            data_object["email"] = request.form['email']
+            return {"data":data_object}
+        else:
+            return {"data": "Not Found"},404
+
